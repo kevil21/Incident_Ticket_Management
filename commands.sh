8e432f2587df2c26159316c68a2adf5c860eab2d
Kevil Patel <kevilpatel24@gmail.com>
2025-11-05 10:36:25 +0530

    v1.0.0 Cleaned Data

---
+++ commands.sh
@@ -0,0 +1,21 @@
+kevil@NVWWMK-662FF940 Incident_Ticket_Management % fish
+Welcome to fish, the friendly interactive shell
+Type help for instructions on how to use fish
+kevil@NVWWMK-662FF940 ~/L/C/O/D/K/T/0/Incident_Ticket_Management (main)> ls
+LICENSE    README.md
+kevil@NVWWMK-662FF940 ~/L/C/O/D/K/T/0/Incident_Ticket_Management (main)> touch .gitignore
+kevil@NVWWMK-662FF940 ~/L/C/O/D/K/T/0/Incident_Ticket_Management (main)> touch instructions.txt
+kevil@NVWWMK-662FF940 ~/L/C/O/D/K/T/0/Incident_Ticket_Management (main)> touch index_data.py
+kevil@NVWWMK-662FF940 ~/L/C/O/D/K/T/0/Incident_Ticket_Management (main)> 
+kevil@NVWWMK-662FF940 ~/L/C/O/D/K/T/0/Incident_Ticket_Management (main)> 
+kevil@NVWWMK-662FF940 ~/L/C/O/D/K/T/0/Incident_Ticket_Management (main)> ls
+index_data.py     instructions.txt  LICENSE           README.md
+kevil@NVWWMK-662FF940 ~/L/C/O/D/K/T/0/Incident_Ticket_Management (main)> mkdir data
+kevil@NVWWMK-662FF940 ~/L/C/O/D/K/T/0/Incident_Ticket_Management (main)> touch commands.sh
+kevil@NVWWMK-662FF940 ~/L/C/O/D/K/T/0/Incident_Ticket_Management (main)> 
+kevil@NVWWMK-662FF940 ~/L/C/O/D/K/T/0/Incident_Ticket_Management (main)> touch commands.sh
+kevil@NVWWMK-662FF940 ~/L/C/O/D/K/T/0/Incident_Ticket_Management (main)> mkdir visualizations
+kevil@NVWWMK-662FF940 ~/L/C/O/D/K/T/0/Incident_Ticket_Management (main)> ls
+commands.sh       data/             data.ipynb        index_data.py     instructions.txt  LICENSE           README.md         visualizations/
+kevil@NVWWMK-662FF940 ~/L/C/O/D/K/T/0/Incident_Ticket_Management (main)> 
+
